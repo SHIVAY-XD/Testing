09d95926c88d53519fa4eb1b6e630c8e5d736d9a
S ʜ ɪ ᴠ ᴀ ʏ <115808731+SHIVAY-XD@users.noreply.github.com>
2024-10-16 02:45:58 +0530

    Update site.py

--- site.py
+++ site.py
@@ -1,15 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://fetchfile.me/en/download-xhamster/?url=https://xhamster.com/videos/stepmom-make-video-for-instagram-and-her-stepson-helps-her-xhey0CY'  # Replace with the actual page URL
+# The URL of the page containing the video
+url = 'https://dirpy.com/studio?url=https://xhamster.com/videos/stepmom-make-video-for-instagram-and-her-stepson-helps-her-xhey0CY&affid=tubeoffline&utm_source=tubeoffline&utm_medium=download'
+
+# Send a request to fetch the page content
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
-# Adjust the selector to find the download link
-download_link = soup.find('a', text='Download')  # Modify based on actual text or attributes
+# Find the video link
+# You may need to adjust this based on the actual HTML structure
+video_tag = soup.find('source')  # Example: looking for a <source> tag in <video>
 
-if download_link:
-    video_url = download_link['href']
-    print("Download link:", video_url)
+if video_tag and 'src' in video_tag.attrs:
+    video_url = video_tag['src']
+    print("Video URL:", video_url)
 else:
-    print("Download link not found.")
+    print("Video link not found.")
