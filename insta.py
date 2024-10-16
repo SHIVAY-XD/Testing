639aae305788601388630f63389a33c8c8181de8
S ʜ ɪ ᴠ ᴀ ʏ <115808731+SHIVAY-XD@users.noreply.github.com>
2024-10-16 21:36:13 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -114,7 +114,7 @@ async def broadcast(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 failed += 1
         
         total_users = len(user_ids)
-        await update.message.reply_text(f"Broadcast complete: {successful} successful, {failed} failed, out of {total_users} total users.")
+        await update.message.reply_text(f"Broadcast complete: \n\nsuccesfully:{successful}, \n\n Failed: {failed} ,\n\nTotal users: {total_users}")
     else:
         await update.message.reply_text("Please reply to a message to broadcast it.")
 
